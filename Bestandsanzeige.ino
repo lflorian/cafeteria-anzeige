89cf996e22d77e2afaa4c4df68d8c884dccfef54
Florian Lammert <contact.lflorian@icloud.com>
2024-10-29 11:41:24 +0100

    Update Bestandsanzeige.ino
    - start up screen
    - foundation for product picker

--- Bestandsanzeige.ino
+++ Bestandsanzeige.ino
@@ -10,40 +10,49 @@ const int colorB = 200;
 int adcPin = A0; // select the input pin for the potentiometer
 int ledPin = A1; // select the pin for the LED
 int adcIn = 0;   // variable to store the value coming from the sensor
-int previousSchieberegler = 0; // comparison value for adcIn
-String printMessage = "Croissant: ";
+int previousQuantitySelector = 0; // comparison value for adcIn
+String ProductName1 = "Croissant: ";
+String ProductName2 = "Hotdog: ";
 
 void setup() {
-  Serial.begin(9600); // init serial to 9600b/s
-  pinMode(ledPin, OUTPUT); // set ledPin to OUTPUT
-
   lcd.begin(16, 2);
   lcd.setRGB(colorR, colorG, colorB);
-  //lcd.print("Hackdays");
-  // Print a message to the LCD.
+  lcd.println("Starting .          ");
+  delay(500);
+  lcd.clear();
+  lcd.println("Starting ..          ");
+  delay(500);
+  lcd.clear();
+  lcd.println("Starting ...          ");
+  delay(2000);
+  lcd.clear();
+  
+  Serial.begin(9600); // init serial to 9600b/s
+  pinMode(ledPin, OUTPUT); // set ledPin to OUTPUT
+  lcd.clear();
 }
 
 void loop() {
   adcIn = analogRead(adcPin);
-  int schieberegler = map(adcIn, 0, 1023, 0, 20); 
+  int QuantitySelector = map(adcIn, 0, 1023, 0, 20); 
 
   //Serial.print(printMessage);
-  //Serial.println(schieberegler);
+  //Serial.println(Anzahl);
   
   lcd.setCursor(0, 0);
   String leerzeichen = "       ";
   String leer = "keine";
-  if (schieberegler > 0){
-  lcd.println(printMessage+schieberegler+leerzeichen);
+  if (QuantitySelector > 0){
+  lcd.println(ProductName1+QuantitySelector+leerzeichen);
   } else {
-  lcd.println(printMessage+leer+leerzeichen);
+  lcd.println(ProductName1+leer+leerzeichen);
   }
 
-  if (schieberegler != previousSchieberegler){
+  if (QuantitySelector != previousQuantitySelector){
   lcd.clear();
-  previousSchieberegler = schieberegler;
-  //Serial.println("schieberegler changed");
-  //Serial.println(previousSchieberegler);
+  previousQuantitySelector = QuantitySelector;
+  //Serial.println("QuantitySelector changed");
+  //Serial.println(previousQuantitySelector);
   } else {
   delay(1000);
   //Serial.println("Nothing changed. Refreshing...");
