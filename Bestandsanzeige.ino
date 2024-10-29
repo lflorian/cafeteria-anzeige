e0678dde9c8adef3fc0ddac2cff542a95998667f
Florian Lammert <contact.lflorian@icloud.com>
2024-10-29 11:21:41 +0100

    Update Bestandsanzeige.ino
    - display only refreshes when value is changed
    - mapping makes scaling of potentiometer easier when there are slight changes in voltage
    - Added blanks to avoid weird characters created by electrical interference

--- Bestandsanzeige.ino
+++ Bestandsanzeige.ino
@@ -7,20 +7,45 @@ const int colorR = 200;
 const int colorG = 200;
 const int colorB = 200;
 
+int adcPin = A0; // select the input pin for the potentiometer
+int ledPin = A1; // select the pin for the LED
+int adcIn = 0;   // variable to store the value coming from the sensor
+int previousSchieberegler = 0; // comparison value for adcIn
+String printMessage = "Croissant: ";
+
 void setup() {
-    // set up the LCD's number of columns and rows:
-    lcd.begin(16, 2);
+  Serial.begin(9600); // init serial to 9600b/s
+  pinMode(ledPin, OUTPUT); // set ledPin to OUTPUT
 
-    lcd.setRGB(colorR, colorG, colorB);
-
-    // Print a message to the LCD.
-    lcd.print("Kaesebroetchen");
-
-    delay(1000);
+  lcd.begin(16, 2);
+  lcd.setRGB(colorR, colorG, colorB);
+  //lcd.print("Hackdays");
+  // Print a message to the LCD.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int quantity1 = 1
-  lcd.print("\(quantity1)");
+  adcIn = analogRead(adcPin);
+  int schieberegler = map(adcIn, 0, 1023, 0, 20); 
+
+  //Serial.print(printMessage);
+  //Serial.println(schieberegler);
+  
+  lcd.setCursor(0, 0);
+  String leerzeichen = "       ";
+  String leer = "keine";
+  if (schieberegler > 0){
+  lcd.println(printMessage+schieberegler+leerzeichen);
+  } else {
+  lcd.println(printMessage+leer+leerzeichen);
+  }
+
+  if (schieberegler != previousSchieberegler){
+  lcd.clear();
+  previousSchieberegler = schieberegler;
+  //Serial.println("schieberegler changed");
+  //Serial.println(previousSchieberegler);
+  } else {
+  delay(1000);
+  //Serial.println("Nothing changed. Refreshing...");
+  }
 }
