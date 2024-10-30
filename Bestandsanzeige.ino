bc419c8b8f18734698ff5a3bedeaf869ee9d27ae
Florian Lammert <contact.lflorian@icloud.com>
2024-10-30 11:03:46 +0100

    Update Bestandsanzeige.ino

--- Bestandsanzeige.ino
+++ Bestandsanzeige.ino
@@ -3,17 +3,15 @@
 
 rgb_lcd lcd;
 
-const int colorR = 200;
-const int colorG = 200;
-const int colorB = 200;
-
-int adc1Pin = A0; // select the input pin for the potentiometer
-int adc2Pin = A1;
-int ledPin = A1; // select the pin for the LED
-int adc1In = 0;   // variable to store the value coming from the sensor
-int adc2In = 0;   // variable to store the value coming from the sensor
+//Pin Initialization
+int adc1Pin = A0; // input pin for potentiometer
+int adc2Pin = A2;
+int LED1Pin = A1; // input pin for LED
+int LED2Pin = A3;
+int adc1In = 0;   // store the value coming from sensor
+int adc2In = 0;
 int previousQuantitySelector = 0; // comparison value for adc1In
-int previousProductSelector = 0; // comparison value for adc2In
+int previousProductSelector = 0;
 String leerzeichen = "       ";
 String leer = "keine";
 
@@ -21,39 +19,63 @@ struct Product {
   String ProductName;
   int quantity;
 };
-
 Product products[] = {
   {"Croissants: ", 20},
   {"Hotdogs: ", 20}
 };
 
+
 void setup() {
+  //Startup Screen
   lcd.begin(16, 2);
-  lcd.setRGB(colorR, colorG, colorB);
+  lcd.setRGB(200, 200, 200);
   lcd.println("Starting .          ");
-  delay(500);
+  delay(250);
   lcd.clear();
   lcd.println("Starting ..          ");
-  delay(500);
+  delay(250);
   lcd.clear();
   lcd.println("Starting ...          ");
-  delay(500);
+  delay(250);
   lcd.clear();
+  refreshDisplay();
 
+  //LED Initalization
+  pinMode(LED2Pin, OUTPUT); // set ledPin to OUTPUT
+  pinMode(LED1Pin, OUTPUT); // set ledPin to OUTPUT
+
+  //Debug
   Serial.begin(9600); // init serial to 9600b/s
-  pinMode(ledPin, OUTPUT); // set ledPin to OUTPUT
   lcd.clear();
 }
 
 void refreshDisplay() {
   Serial.println("Refresh Display");
   lcd.clear();
-  
-  lcd.setCursor(0, 0);
-  lcd.println(products[0].ProductName+String(products[0].quantity)+leerzeichen);
-  lcd.setCursor(0, 1);
-  lcd.println(products[1].ProductName+String(products[1].quantity)+leerzeichen);
+
+  if ( products[0].quantity == 0 ) {
+    lcd.setCursor(0, 0);
+    lcd.println("Croissants:keine" + leerzeichen);
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.println(products[0].ProductName + String(products[0].quantity) + leerzeichen);
   }
+
+  if ( products[1].quantity == 0 ) {
+    lcd.setCursor(0, 1);
+    lcd.println("Hotdogs: keine" + leerzeichen);
+  } else {
+    lcd.setCursor(0, 1);
+    lcd.println(products[1].ProductName + String(products[1].quantity) + leerzeichen);
+  }
+
+  if (products[0].quantity == 0 || products[1].quantity == 0) {
+    lcd.setRGB(250, 0, 0);
+  } else {
+    lcd.setRGB(200, 200, 200);
+  }
+}
+
 
 void loop() {
   //Slider Mapping
